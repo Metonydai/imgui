4132242ddcf5ebe83445213d64594ea179f63e9e
Metonydai <kusuje@gmail.com>
2025-06-23 23:41:04 +0800

    Updated the premake file

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,6 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -24,11 +23,13 @@ project "ImGui"
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
+		staticruntime "on"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
 		cppdialect "C++17"
+		staticruntime "on"
 
 	filter "configurations:Debug"
 		runtime "Debug"
@@ -41,4 +42,3 @@ project "ImGui"
     filter "configurations:Dist"
 		runtime "Release"
 		optimize "on"
-        symbols "off"
